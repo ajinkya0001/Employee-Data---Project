a80f1928bd3da57c4acfdd264fad0ae91652e995
ajinkya0001 <jadkarajinkya@gmail.com>
2026-04-05 03:30:25 +0530

    Home Page Added with View employees and edit and delete operation.

--- src/main/java/com/employee/controller/EmployeeController.java
+++ src/main/java/com/employee/controller/EmployeeController.java
@@ -2,13 +2,24 @@ package com.employee.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.employee.service.EmployeeService;
+import com.employee.EmployeeManagementCrudApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import com.employee.entity.Employee;
+import java.util.List;
+import org.springframework.ui.Model;
 
 @Controller
 @RequestMapping("/employee")
 public class EmployeeController {
+
+//    private final EmployeeManagementCrudApplication employeeManagementCrudApplication;
 	
 	@Autowired
 	EmployeeService empService;
@@ -18,6 +29,50 @@ public class EmployeeController {
 		return "Add-Employee";
 	}
 	
+	@PostMapping("/add")
+	public String addEvent(@ModelAttribute Employee employee) {
+		
+		empService.saveEmployee(employee);
+		return "redirect:/employee/view-employees";
+		
+	}
 	
+	@GetMapping("/view-employees")
+	public String viewEmployees(Model model) {
+		List<Employee> elist = empService.getAllEmployees();
+		
+		model.addAttribute("employeeList", elist);
+		return "View-Employees";
+	}
+	
+	
+	@GetMapping("/edit/{id}")
+	public String getEditForm(@PathVariable int id, Model model) { // Path variable get mapping into local variable  @PathVariable("id")
+		Employee existingEmployee = empService.getEmployeeById(id);  // existing event
+		model.addAttribute("employee", existingEmployee);
+		return "Edit-Form";
+	}
+	
+	@GetMapping("/delete/{id}")
+	public String deleteEvent(@PathVariable int id ) {
+		empService.deleteEmployee(id);
+		return "redirect:/event/view-events";
+	}
+	
+	@GetMapping("/")
+	public String home() {
+		return "Home";
+	}
 
+	@GetMapping("/search")
+	public String searchEmployee(@RequestParam("eid") int eid, RedirectAttributes model) {   // RedirectAttributes Sub interface of Model Interface it is used to rediect to the home page  
+		
+		Employee employee = empService.getEmployeeById(eid);
+		if(employee!=null) {
+			model.addFlashAttribute("employee", employee);
+		}else {
+			model.addFlashAttribute("notFound", "Employee Not Found With id : " + eid);
+		}
+		return "redirect:/employee/";
+	} 
 }
